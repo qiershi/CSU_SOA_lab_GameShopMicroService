5f3784c95f47b8c0e979198c020a69a2e6a7b581
Jiyuchong <Jiyuchong@csu.edu.cn>
2025-11-22 09:38:56 +0800

    合并product-service

---
+++ product-service/src/main/java/com/csu/productservice/mapper/EditionMapper.java
@@ -0,0 +1,29 @@
+package com.csu.productservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.csu.productservice.entity.Edition;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Component
+@Repository()
+@Mapper
+public interface EditionMapper extends BaseMapper<Edition> {
+    @Insert({
+            "<script>",
+            "INSERT INTO item_edition (product_id, editionName, price, storage)",
+            "VALUES ",
+            "<foreach collection='list' item='item' separator=','>",
+            "(",
+            "#{item.productId}, #{item.editionName}, #{item.price}, #{item.storage}",
+            ")",
+            "</foreach>",
+            "</script>"
+    })
+    int insertBatch(@Param("list") List<Edition> editions); // 使用 @Param("list") 绑定集合参数
+}
